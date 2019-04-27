c2368e575c8bac677eaaf7569623400753dd807a
kazeroz <49557307+kazeroz@users.noreply.github.com>
2019-04-27 18:08:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return '<html><head><script type="text/javascript">var text = document.getElementsByTagName("input")[0];var val=text.innerHTML;alert(val)</script></head><body><form action="#"><input type="text" value="Some text here..." /><input type="submit" /></form></body></html>'
+    return '<html><head><script type="text/javascript">var text = document.getElementsByTagName("input")[0];var val = text.value;alert(val);</script></head><body><form action="#"><input type="text" value="Some text here..." /><input type="submit" /></form></body></html>'
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', debug=True)

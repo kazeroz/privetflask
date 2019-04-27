125b7cdcf24bed9d919e68d1966d537539febd1d
kazeroz <49557307+kazeroz@users.noreply.github.com>
2019-04-27 17:35:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return 'ZDAROVA DENIS!!!! '
-
+cont= {}
+for s in check_string:
+    if count.has_key(s):
+        count[s]+=1
+        else:
+            count[s]=1
+            for key in count :
+                if count[key] >1:
+                    print key, count[key]
 if __name__ == '__main__':
  app.run(host='0.0.0.0', debug=True)

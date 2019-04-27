6877855bbb364d99558527f11a36cc6eb9c5c11d
kazeroz <49557307+kazeroz@users.noreply.github.com>
2019-04-27 17:56:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for s in check_string:
     if count.has_key(s):
         count[s]+=1
         else:
-            count[s]=1
+            count[s]=1:
             for key in count :
                 if count[key] >1:
                     print key, count[key]:
